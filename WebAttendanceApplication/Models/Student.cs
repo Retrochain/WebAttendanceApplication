97d35c72b508c67036ab37e53340a3232589ca2b
Retro_chain <94081759+Retrochain@users.noreply.github.com>
2025-03-22 23:35:35 -0500

    Created a login MVC (beta)

---
+++ WebAttendanceApplication/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿namespace WebAttendanceApplication.Models;
+
+public partial class Student
+{
+    public int StudentId { get; set; }
+
+    public string UtdId { get; set; } = null!;
+
+    public string? FirstName { get; set; }
+
+    public string? MiddleInit { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string UserName { get; set; } = null!;
+
+    public string? Password { get; set; }
+
+    public DateTime? CreatedOn { get; set; }
+
+    public virtual ICollection<Attendance> Attendances { get; set; } = new List<Attendance>();
+
+    public virtual ICollection<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
+
+    public virtual ICollection<StudentAnswer> StudentAnswers { get; set; } = new List<StudentAnswer>();
+}
